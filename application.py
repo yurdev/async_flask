8468ae3a50a364725f5c464c374319f5cd6c319c
github <noreply@github.com>
2020-10-03 23:12:42 +0900

    Multiple improvements to make thread stop when no clients but restart on a new client

--- application.py
+++ application.py
@@ -5,6 +5,7 @@ Aim is to create a webpage that is constantly updated with random numbers from a
 from flask_socketio import SocketIO
 from flask import Flask, render_template
 from random import random
+import threading
 from threading import Thread, Event
 
 app = Flask(__name__)
@@ -12,7 +13,7 @@ app.config['SECRET_KEY'] = 'secret!'
 app.config['DEBUG'] = True
 
 # Turn the flask app into a SocketIO app
-socket_io = SocketIO(app, async_mode=None, logger=False, engineio_logger=False)
+socket_io = SocketIO(app, async_mode='eventlet', logger=False, engineio_logger=False)
 
 # Random number Generator Thread
 thread = Thread()
@@ -27,9 +28,10 @@ def random_number_generator():
     """
     while not thread_stop_event.isSet():
         number = round(random() * 10, 3)
-        print(number)
+        print(f'{number}, thread_ident={threading.get_ident()}')
         socket_io.emit('new_number', {'number': number}, namespace='/test')
         socket_io.sleep(5)
+    print('Thread is done')
 
 
 @app.route('/')
@@ -44,24 +46,28 @@ def test_connect():
     global thread
     global client_counter
     client_counter += 1
-    print(f'Clients connected: {client_counter}')
+    print(f'Clients connected: {client_counter}, thread_ident={threading.get_ident()}')
+
+    if thread_stop_event.is_set():
+        thread_stop_event.clear()
 
     # Start the random number generator thread only if the thread has not been started before.
-    if not thread.isAlive():
-        print("Starting Thread")
-        thread_stop_event.clear()
+    if not thread.is_alive():
+        print(f"Starting Thread, thread_ident={threading.get_ident()}")
         thread = socket_io.start_background_task(random_number_generator)
+    elif client_counter == 1:
+        print('Continue use existing thread that is still alive')
 
 
 @socket_io.on('disconnect', namespace='/test')
 def test_disconnect():
     global client_counter
     client_counter -= 1
-    print(f'Client disconnected, left: {client_counter}')
+    print(f'Client disconnected, left: {client_counter}, thread_ident={threading.get_ident()}')
     if thread.is_alive() and client_counter == 0:
         global thread_stop_event
         thread_stop_event.set()
-        print('Disconnected & thread stopped')
+        print('Set event to stop thread')
 
 
 if __name__ == '__main__':
